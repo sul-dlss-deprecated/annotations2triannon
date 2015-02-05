9e451a1e7316c3746d13fa60bf775c3f3b2f432a
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2015-02-04 17:51:12 -0800

    Add basic config and console

--- lib/annotations2triannon.rb
+++ lib/annotations2triannon.rb
@@ -1,2 +1,56 @@
-class Annotations2triannon
+require 'dotenv'
+Dotenv.load
+
+require 'pry'
+require 'pry-doc'
+
+require 'json'
+require 'linkeddata'
+require 'ruby-progressbar'
+
+require_relative 'annotations2triannon/configuration'
+
+
+module Annotations2triannon
+
+  # configuration at the module level, see
+  # http://brandonhilkert.com/blog/ruby-gem-configuration-patterns/
+
+  class << self
+    attr_writer :configuration
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.reset
+    @configuration = Configuration.new
+  end
+
+  def self.configure
+    yield(configuration)
+  end
+
+  def self.http_head_request(url)
+    uri = URI.parse(url)
+    begin
+      if RUBY_VERSION =~ /^1\.9/
+        req = Net::HTTP::Head.new(uri.path)
+      else
+        req = Net::HTTP::Head.new(uri)
+      end
+      Net::HTTP.start(uri.host, uri.port) {|http| http.request req }
+    rescue
+      @configuration.logger.error "Net::HTTP::Head failed for #{uri}"
+      begin
+        Net::HTTP.get_response(uri)
+      rescue
+        @configuration.logger.error "Net::HTTP.get_response failed for #{uri}"
+        nil
+      end
+    end
+  end
+
 end
+
---
+++ lib/annotations2triannon/configuration.rb
@@ -0,0 +1,38 @@
+
+module Annotations2triannon
+
+  class Configuration
+
+    attr_accessor :debug
+
+    def initialize
+      @debug = env_boolean('DEBUG')
+
+      # logging
+      log_file = ENV['LOG_FILE'] || 'annotations2triannon.log'
+      log_file = File.absolute_path log_file
+      @log_file = log_file
+      log_path = File.dirname log_file
+      unless File.directory? log_path
+        # try to create the log directory
+        Dir.mkdir log_path rescue nil
+      end
+      begin
+        log_file = File.new(@log_file, 'w+')
+      rescue
+        log_file = $stderr
+        @log_file = 'STDERR'
+      end
+      @logger = Logger.new(log_file, shift_age = 'monthly')
+      @logger.level = @debug ? Logger::DEBUG : Logger::INFO
+    end
+
+    def env_boolean(var)
+      # check if an ENV variable is true, use false as default
+      ENV[var].to_s.upcase == 'TRUE' rescue false
+    end
+
+  end
+
+end
+
