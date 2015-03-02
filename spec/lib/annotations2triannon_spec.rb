07f4412bf4e5526b6360b0bfe7e092dcc48b9ef2
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2015-03-02 15:32:59 -0800

    Enhance config tests

--- spec/lib/annotations2triannon_spec.rb
+++ spec/lib/annotations2triannon_spec.rb
@@ -1,9 +1,45 @@
 require 'spec_helper'
 
 describe Annotations2triannon do
+
   describe ".configuration" do
     it "should be a configuration object" do
       expect(described_class.configuration).to be_a_kind_of Annotations2triannon::Configuration
     end
   end
+
+  describe "#configure" do
+    before :each do
+      Annotations2triannon.configure do |config|
+        config.debug = true
+      end
+    end
+    it "returns a hash of options" do
+      config = Annotations2triannon.configuration
+      expect(config).to be_instance_of Annotations2triannon::Configuration
+      expect(config.debug).to be_truthy
+    end
+    after :each do
+      Annotations2triannon.reset
+    end
+  end
+
+  describe ".reset" do
+    before :each do
+      Annotations2triannon.configure do |config|
+        config.debug = true
+      end
+    end
+    it "resets the configuration" do
+      Annotations2triannon.reset
+      config = Annotations2triannon.configuration
+      expect(config).to be_instance_of Annotations2triannon::Configuration
+      expect(config.debug).to be_falsey
+    end
+    after :each do
+      Annotations2triannon.reset
+    end
+  end
+
 end
+
